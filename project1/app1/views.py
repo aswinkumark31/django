47079c5b2e01d46eb401ddecc0aee0d84aa8e787
aswin <aswinsynnefo3132001@gmail.com>
2023-09-14 15:57:55 +0530

    product site

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 def about(request):
-    return render(request,'about.html')
+    d= {
+        'data':pro.objects.all()
+        }
+    return render(request,'about.html',d)
+def contact(request):
+    return render(request,'contact.html')
